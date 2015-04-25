38199dcfbc4c04768e08ba22b2afef8891aa5c46
takeshi yamashita <yamashita_takeshi@cyberagent.co.jp>
2015-04-25 21:26:07 +0900

    testing

---
+++ main_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestMainHelp(t *testing.T) {
+
+	Convey("Ginven no arges", t, func() {
+		Convey("When called", func() {
+			// TODO
+			Convey("Then ok", func() {
+
+			})
+		})
+	})
+}
